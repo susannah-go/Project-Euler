29486ee8db7de81880c0e5cb08d4c731d7934e1e
Susannah <sgo@unomaha.edu>
2015-10-24 19:14:04 -0500

    Problem 2 solution
    Problem 2 solution

---
+++ ProjectEuler2.java
@@ -0,0 +1,40 @@
+// solution to Project Euler Problem 1
+
+public class ProjectEuler2 {
+
+    public static void main(String[] args) {
+
+        int sum = 0;
+        int n = 1;
+
+        int value1 = 1;
+        int value2 = 2;
+        int fib;
+
+        while(true) {
+
+            // get nth Fibonacci number
+            if(n <= 2) {
+                fib = n;
+            } else {
+                fib = value1 + value2;
+                value1 = value2;
+                value2 = fib;
+            }
+
+            if(fib > 4000000) {
+                break;
+            }
+
+            if(fib % 2 == 0) {
+                sum += fib;
+            }
+
+            n++;
+        }
+
+        System.out.println(sum);
+
+    }
+}
+
